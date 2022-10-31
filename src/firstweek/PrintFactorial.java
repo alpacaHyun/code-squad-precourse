d35639cb2f632e6c4fae7561b2917bf3161715e7
Donghyun <flaehan@gmail.com>
2022-10-31 22:31:27 +0900

    숫자의 합

---
+++ src/firstweek/PrintFactorial.java
@@ -0,0 +1,20 @@
+package firstweek;
+
+import java.util.Scanner;
+
+public class PrintFactorial {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String a = sc.nextLine();
+        sc.close();
+
+        int sum = 0;
+
+        for(int i = 0; i < N; i++){
+            sum += a.charAt(i)-'0';
+        }
+        System.out.println(sum);
+
+    }
+}
