cb4ef79221c1083cd348ec39e86754ae461f7e06
Donghyun <flaehan@gmail.com>
2022-10-31 17:55:28 +0900

    45분 일찍 깨워주는 알람

---
+++ src/firstweek/Alarm.java
@@ -0,0 +1,23 @@
+package firstweek;
+
+import java.util.Scanner;
+
+public class Alarm {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+
+        if((M-45) < 0){
+            if(H == 0){
+                System.out.println((23) + " " + (M+15));
+            }if(H > 0) {
+                System.out.println((H - 1) + " " + (M+15));
+            }
+        }
+        if((M-45) >= 0){
+            System.out.println(H + " " + (M-45));
+        }
+    }
+}
