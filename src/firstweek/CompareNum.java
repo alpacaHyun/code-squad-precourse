720effeb261b1e39a52557424c47ea95bf6f4a59
Donghyun <flaehan@gmail.com>
2022-10-31 16:42:24 +0900

    백준 제출용 수정

--- src/firstweek/CompareNum.java
+++ src/firstweek/CompareNum.java
@@ -5,11 +5,9 @@ import java.util.Scanner;
 public class CompareNum {
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("두 수를 입력하세요 : ");
 
         int A = sc.nextInt();
         int B = sc.nextInt();
-        System.out.println(A + " " + B);
 
         if(A > B){
             System.out.println(">");
