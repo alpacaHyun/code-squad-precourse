f1ed396140b2e86b471066c29799a38a142459c3
Donghyun <flaehan@gmail.com>
2022-10-31 16:35:14 +0900

    시험 성적 확인

--- src/firstweek/CompareNum.java
+++ src/firstweek/CompareNum.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class CompareNum {
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
-
         System.out.println("두 수를 입력하세요 : ");
 
         int A = sc.nextInt();
---
+++ src/firstweek/TestScore.java
@@ -0,0 +1,36 @@
+package firstweek;
+
+public class TestScore {
+    public static String Grade(int testScore){
+        String result = null;
+
+        if(90<=testScore && testScore<=100){
+            result = "A";
+            System.out.println(result);
+        }
+        if(80<=testScore && testScore<=89){
+            result = "B";
+            System.out.println(result);
+        }
+        if(70<=testScore && testScore<=79){
+            result = "C";
+            System.out.println(result);
+        }
+        if(60<=testScore && testScore<=69){
+            result = "D";
+            System.out.println(result);
+        }
+        if(0<=testScore && testScore<=59){
+            result = "F";
+            System.out.println(result);
+        }
+        return result;
+    }
+    public static void main(String [] args){
+        int testScore = 100;
+
+        Grade(testScore);
+
+
+    }
+}
