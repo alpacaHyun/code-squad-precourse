4c2559a3d9768bfaead875c41b850e05109e988c
Donghyun <flaehan@gmail.com>
2022-11-01 17:07:42 +0900

    깔끔하게 수정

--- src/precourse/firstweek/mon/MultiplicationTable.java
+++ src/precourse/firstweek/mon/MultiplicationTable.java
@@ -3,8 +3,10 @@ package precourse.firstweek.mon;
 public class MultiplicationTable {
     public static void main(String [] args){
         for(int i = 1; i <= 9; i++){
+            System.out.println("**** i단 ***");
             for (int j = 1; j <= 9; j++){
                 System.out.printf("%d x %d = %d  ",i,j,i*j);
+                System.out.println(" ");
             }System.out.println(" ");
         }
     }
