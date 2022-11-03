9c2d7febd4c1d32e504d8db7f63b391eff2a929b
Donghyun <flaehan@gmail.com>
2022-11-03 23:49:39 +0900

    type : refactor
    body :

---
+++ src/precourse/firstweek/algorithm/OXQuiz.java
@@ -0,0 +1,20 @@
+package precourse.firstweek.algorithm;
+
+public class OXQuiz {
+    public static void main(String [] args){
+
+    }
+
+}
+/*
+1. 테스트 케이스 입력
+2. OX 입력
+3. O 면 기본값 1 주고
+4. 연속되는 O는 +1 씩
+5. if X = 점수 0으로
+6. 더하는 점수들 int 배열로 만들기
+7. 배열 합하기
+
+
+
+ */
---
+++ src/precourse/firstweek/algorithm/Scale.java
@@ -0,0 +1,61 @@
+//package precourse.firstweek.algorithm;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//
+//public class Scale {
+//    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//    public void main(String [] args){
+//
+//    conclusion();
+//    }
+//
+//    // 배열 만들기
+//    public void makeArray[](String p)throws IOException {
+//
+//        String p = br.readLine();
+//        String [] temp = p.split(" ");
+//
+//        int [] num = new int [temp.length];
+//        for(int i = 0; i < temp.length; i++){
+//            num[i] = Integer.parseInt(temp[i]);
+//        }
+//    }
+//    //연속되는지 확인
+//    public void int checkContinue(){
+//        boolean con = true;
+////앞 수와 뒷 수 비교해서 ascending descending mixed 구분
+//            for(int i = 0; i<array.length(); i++){
+//                for(int j = 0; j <array.length()-1; j++){
+//                    if(num[i] > num[j]){
+//
+//                        continue;
+//                    }else{
+//                        break;
+//                    }
+//                }
+//            }
+//            if (con = false){
+//
+//            }
+//
+//
+//        return 0;
+//    }
+//    public String conclusion(int a){
+//        String answer = null;
+//        if (checkContinue().equals.ascending){
+//            System.out.println("ascending");
+//        }
+//        if(checkContinue().equals(descending)){
+//            System.out.println("descending");
+//        }
+//        if(checkContinue().equals(mixed)){
+//            System.out.println("mixed");
+//        }
+//        return answer;
+//    }
+//
+//}
