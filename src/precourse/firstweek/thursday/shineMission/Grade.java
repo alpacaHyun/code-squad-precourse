4428bef71824eafbdce02aa7a49b5738fd4cde73
Donghyun <flaehan@gmail.com>
2022-11-06 12:55:49 +0900

    type : feat 평균 산출
    body : shine 추가미션

---
+++ src/precourse/firstweek/thursday/shineMission/Grade.java
@@ -0,0 +1,30 @@
+package precourse.firstweek.thursday.shineMission;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Grade {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("수학, 과학, 영 순으로 3개의 성적 입력. " + "\n");
+        bw.flush();
+
+        int mathScore = Integer.parseInt(br.readLine());
+        int scienceScore = Integer.parseInt(br.readLine());
+        int englishScore = Integer.parseInt(br.readLine());
+
+        bw.write("평균은 : " + averageScore(mathScore, scienceScore, englishScore) + "\n");
+        bw.flush();
+        bw.close();
+    }
+
+    private static int averageScore(int a, int b, int c) {
+        return (a + b + c) / 3;
+    }
+}
