5dd2cc97da945466ffcbe817f8af5a359fa42ae4
Donghyun <flaehan@gmail.com>
2022-11-10 17:05:54 +0900

    type : feat 미션 2 도전
    body :

---
+++ src/precourse/secondweek/thursday/mission2/File.java
@@ -0,0 +1,4 @@
+package precourse.secondweek.thursday.mission2;
+
+public class File {
+}
---
+++ src/precourse/secondweek/thursday/mission2/Student.java
@@ -0,0 +1,20 @@
+//package precourse.secondweek.thursday.mission2;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Student {
+//
+//   private  List<Student> studentList = new ArrayList<Student>();
+//
+//
+//    public List<Student> studentList(){
+//        if(!studentList.contains(String "Kim", String "Lee")){
+//            return studentList.add(String Kim);
+//        }
+//
+//
+//
+//        return studentList;
+//    }
+//}
