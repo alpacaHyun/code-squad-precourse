f263002d2f2e10aa54685027680ca943ae7eb348
alpacaHyun <flaehdan@gmail.com>
2022-11-15 15:42:13 +0900

    type : refactor
    body :

--- src/precourse/thirdweek/mon/mission1/MemberTest.java
+++ src/precourse/thirdweek/mon/mission1/MemberTest.java
@@ -6,7 +6,7 @@ public class MemberTest {
 
         Member memberJames = new MemberRED("James", 10000, 2);
         Member memberTomas = new MemberRED("Tomas", 30000, 2);
-//
+
         Member memberEdward = new MemberPLATINUM("Edward", 10000, 2);
         Member memberPercy = new MemberPLATINUM("Percy", 30000, 2);
 
@@ -17,5 +17,7 @@ public class MemberTest {
         System.out.println(memberEdward.showMemberInfo());
         System.out.println(memberPercy.showMemberInfo());
         System.out.println(memberElizabet.showMemberInfo());
+
+        // 값을 넣을 때 금액같은 경우 멤버변수 안에 넣는 것이 아니라, showMemberInfo에 넣어도 됨
     }
 }
